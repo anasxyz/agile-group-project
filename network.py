695ae2286f9cd21d54cf1903e155f177fb4cfd8a
Anas Saad <114344706+anasxyz@users.noreply.github.com>
2025-01-22 23:53:41 +0000

    Added Multi-Threading and Logging

--- network.py
+++ network.py
@@ -1,20 +1,37 @@
 from flask import Flask, request, jsonify
+import logging
 
 app = Flask(__name__)
 
-@app.route('/approve', methods=['POST'])
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(message)s",
+    handlers=[
+        logging.StreamHandler(),  
+        logging.FileHandler("network_simulator.log")  
+    ]
+)
+
+@app.route('/simulate', methods=['POST'])
 def simulate_network():
-    try:
+    try: 
         transaction_data = request.json
-                
+        transaction_id = transaction_data.get("transaction_id", "Unknown")
+        
+        logging.info(f"Transaction {transaction_id} received from Transaction Switch")
+        
         response = {
             "status": "approved",
-            "transaction_id": transaction_data.get("transaction_id"),
+            "transaction_id": transaction_id,
             "message": "Transaction authorised successfully."
         }
+ 
+        logging.info(f"Transaction {transaction_id} approved")
+        
         return jsonify(response)
     except Exception as e:
+        logging.error(f"Error in network simulation: {e}")
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(port=5001)
+    app.run(port=5001, threaded=True)

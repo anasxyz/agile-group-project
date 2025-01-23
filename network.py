4b8b69e5982adf0fe07842db3a17b62ac73108ff
Anas Saad <114344706+anasxyz@users.noreply.github.com>
2025-01-23 00:32:22 +0000

    Fixed logging + logging in local file

--- network.py
+++ network.py
@@ -1,36 +1,37 @@
 from flask import Flask, request, jsonify
-import logging
+import datetime
 
 app = Flask(__name__)
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(message)s",
-    handlers=[
-        logging.StreamHandler(),  
-        logging.FileHandler("network_simulator.log")  
-    ]
-)
+def log(message, level="INFO"):
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    log_message = f"{timestamp} - {level} - {message}"
+    
+    print(log_message)  # log in terminal
+
+    # log in file
+    with open('network.log', 'a') as f:
+        f.write(log_message + "\n")
 
 @app.route('/simulate', methods=['POST'])
 def simulate_network():
-    try: 
+    try:
         transaction_data = request.json
         transaction_id = transaction_data.get("transaction_id", "Unknown")
         
-        logging.info(f"Transaction {transaction_id} received from Transaction Switch")
+        log(f"Transaction {transaction_id} received from Transaction Switch")
         
         response = {
             "status": "approved",
             "transaction_id": transaction_id,
             "message": "Transaction authorised successfully."
         }
- 
-        logging.info(f"Transaction {transaction_id} approved")
+        
+        log(f"Transaction {transaction_id} approved")
         
         return jsonify(response)
     except Exception as e:
-        logging.error(f"Error in network simulation: {e}")
+        log(f"Error in network simulation: {e}", level="ERROR")
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':

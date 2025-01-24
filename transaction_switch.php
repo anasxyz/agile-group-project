9c138542d4055b5aa2cb62836b26c39257af49fd
Anas Saad <114344706+anasxyz@users.noreply.github.com>
2025-01-24 04:38:39 +0000

    Working on fix for transaction flow

--- transaction_switch.php
+++ transaction_switch.php
@@ -1,17 +1,16 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // Get the transaction data from the ATM
     $transaction_data = $_POST;
 
-    // Send the transaction data to the Network Simulator
-    $url = 'http://localhost/network_simulator.php'; // The endpoint of the network simulator
+    // Forward the transaction data to the Network Simulator
+    $url = 'http://localhost/network_simulator.php';
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_POST, true);
     curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($transaction_data));
-    $network_response = curl_exec($ch);
+    $response = curl_exec($ch);
     curl_close($ch);
 
-    // Forward the response back to the ATM
-    echo $network_response;
+    // Return the response from the Network Simulator to the ATM
+    echo $response;
 }

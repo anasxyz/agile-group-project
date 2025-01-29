a5878e0ce6dc09548d02ac52e63348253c80d4cd
ahmedyoussef <2507690@dundee.ac.uk>
2025-01-29 22:41:17 +0000

    updated the page to support multiple languages

--- Source Code/ATM/balance_options.php
+++ Source Code/ATM/balance_options.php
@@ -1,34 +1,42 @@
 <?php
 session_start(); // Ensure session is started to use session variables
+
+
+$_SESSION['language'] = 'es';
+$language = $_SESSION['language'] ?? 'en';
+
+$lang = include "../languages/{$language}.php";
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Balance Options</title>
   <link rel="stylesheet" href="styles.css">
 </head>
+
 <body>
   <div class="container">
-    <h1>Balance Inquiry</h1>
-	<h3>Select an Account</h3>
+    <h1><?= $lang['balance_inquiry'] ?></h1>
+    <h3><?= $lang['select_account'] ?></h3>
 
     <div class="option" onclick="redirectToBalanceChoice('checking')">
-      <span>Checking</span>
+      <span><?= $lang['checking'] ?></span>
     </div>
 
     <div class="option" onclick="redirectToBalanceChoice('savings')">
-      <span>Savings</span>
+      <span><?= $lang['savings'] ?></span>
     </div>
-   
+
     <div class="option" onclick="redirectToBalanceChoice('credit')">
-      <span>Credit</span>
+      <span><?= $lang['credit'] ?></span>
     </div>
-  
+
     <div class="option" onclick="redirectToBalanceChoice('loan')">
-      <span>Loan</span>
+      <span><?= $lang['loan'] ?></span>
     </div>
   </div>
 
@@ -45,7 +53,8 @@ session_start(); // Ensure session is started to use session variables
   </div>
 
   <script src="modal.js">
-    
+
   </script>
 </body>
+
 </html>

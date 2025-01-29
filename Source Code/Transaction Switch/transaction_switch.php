9d4e47a9cfd6a917f19758167c96c2e03a9a4c8a
Jamie Till <114414595+JamieT1ll@users.noreply.github.com>
2025-01-29 15:03:07 +0000

    Incoming and Outcoming switch logs

--- Source Code/Transaction Switch/transaction_switch.php
+++ Source Code/Transaction Switch/transaction_switch.php
@@ -8,6 +8,16 @@ header('Access-Control-Allow-Headers: Content-Type, Authorization');
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $transaction_data = $_POST;
 
+    $incomeStmt = $conn->prepare("INSERT INTO TransactionSwitchLogs (CardNumber, TransactionId, Date, Balance, Direction) 
+    VALUES (:cardNumber, :transactionId, CURDATE(), :balance, :direction)");
+    $incomeStmt->bindParam(':cardNumber', $transaction_data['card_number']);
+    $incomeStmt->bindParam(':transactionId', $transaction_data['transaction_id']);
+    $balance = NULL; // No balance yet before sending the request
+    $incomeStmt->bindParam(':balance', $balance);
+    $direction = "NetworkSim"; // Request being sent
+    $incomeStmt->bindParam(':direction', $direction);
+    $incomeStmt->execute();
+
     $url = 'http://localhost/../Network%20Simulator/network_simulator_1.php';
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -26,7 +36,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $logStmt->bindParam(':cardNumber', $transaction_data['card_number']);
     $logStmt->bindParam(':transactionId', $transaction_data['transaction_id']);
     $logStmt->bindParam(':balance', $balance);
-    $direction = "Outgoing"; //Need logic to tell if json is coming from simulator or ATM
+    $direction = "ATM"; //Need logic to tell if json is coming from simulator or ATM
     $logStmt->bindParam(':direction', $direction);
     $logStmt->execute();
 

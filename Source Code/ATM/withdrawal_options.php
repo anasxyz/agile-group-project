0da659d4f0f4827a913d5a76d21ec23a88be214a
ahmedyoussef <2507690@dundee.ac.uk>
2025-01-30 01:57:51 +0000

    updated the page to support multiple langauges

--- Source Code/ATM/withdrawal_options.php
+++ Source Code/ATM/withdrawal_options.php
@@ -1,34 +1,41 @@
 <?php
 session_start(); // Ensure session is started to use session variables
+
+$_SESSION['language'] = 'es';
+$language = $_SESSION['language'] ?? 'en';
+
+$lang = include "../languages/{$language}.php";
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Withdrawal Options</title>
   <link rel="stylesheet" href="styles.css">
 </head>
+
 <body>
   <div class="container">
-    <h1>Withdrawal Options</h1>
-	<h3>Select an Account</h3>
+    <h1><?= $lang['withdrawal_options'] ?></h1>
+    <h3><?= $lang['select_account'] ?></h3>
 
     <div class="option" onclick="redirectToWithdrawalChoice('checking')">
-      <span>Checking</span>
+      <span><?= $lang['checking'] ?></span>
     </div>
 
     <div class="option" onclick="redirectToWithdrawalChoice('savings')">
-      <span>Savings</span>
+      <span><?= $lang['savings'] ?></span>
     </div>
-   
+
     <div class="option" onclick="redirectToWithdrawalChoice('credit')">
-      <span>Credit</span>
+      <span><?= $lang['credit'] ?></span>
     </div>
-  
+
     <div class="option" onclick="redirectToWithdrawalChoice('loan')">
-      <span>Loan</span>
+      <span><?= $lang['loan'] ?></span>
     </div>
   </div>
 
@@ -45,7 +52,8 @@ session_start(); // Ensure session is started to use session variables
   </div>
 
   <script src="modal.js">
-    
+
   </script>
 </body>
+
 </html>

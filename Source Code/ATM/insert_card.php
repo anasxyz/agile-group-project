e5977cd9dd595fb5daa4e314265d07ce74e523b0
Anas Saad <114344706+anasxyz@users.noreply.github.com>
2025-01-31 12:35:08 +0000

    Default currency £

--- Source Code/ATM/insert_card.php
+++ Source Code/ATM/insert_card.php
@@ -9,6 +9,8 @@ unset($_SESSION['pin']);
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+$_SESSION['currencyType'] = "£";
 ?>
 
 <!DOCTYPE html>
